bd1b6fceeb8f47981eea06b447aa1b673059a738
Drew Powers <drew@pow.rs>
2020-12-03 16:48:27 -0700

    Add even more tests

---
+++ test/build/package-tippy-js/package-tippy-js.test.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+const cwd = path.join(__dirname, 'build');
+
+describe('package: tippy.js', () => {
+  it('builds to the correct path', () => {
+    const mainEntry = path.join(cwd, 'web_modules', 'tippyjs.js');
+    const assets = path.join(cwd, 'web_modules', 'tippyjs');
+
+    expect(fs.existsSync(mainEntry) && fs.statSync(mainEntry).isFile()).toBe(true);
+    expect(fs.existsSync(assets) && fs.statSync(assets).isDirectory()).toBe(true);
+  });
+
+  it('resolves imports', () => {
+    const distJS = fs.readFileSync(path.join(cwd, '_dist_', 'index.js'), 'utf-8');
+
+    expect(distJS.includes(`import tippy from '../web_modules/tippyjs.js';`)).toBe(true);
+  });
+});
